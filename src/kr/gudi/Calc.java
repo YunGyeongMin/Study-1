cd3206e15755dc988d8e1d2da57c68733cd2408e
GD6 <GD6@DESKTOP-58VHOFH>
2019-10-30 17:35:17 +0900

    외부 클래스 사용 (계산기 만들기)

---
+++ src/kr/gudi/Calc.java
@@ -0,0 +1,15 @@
+package kr.gudi;
+
+public class Calc {
+	
+	public void 덧셈(int a, int b) {
+		int c = a + b;
+		System.out.println(a + " + " + b + " = " + c);
+	}
+	
+	public void 뺄셈(int a, int b) {
+		int d = a - b;
+		System.out.println(a + " - " + b + " = " + d);
+	}
+
+}
--- src/kr/gudi/Test.java
+++ src/kr/gudi/Test.java
@@ -5,18 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		int a = 5;
 		int b = 3;
-		덧셈(a,b);
-		뺄셈(a,b);
-	}
-	
-	public static void 덧셈(int a, int b) {
-		int c = a + b;
-		System.out.println(a + " + " + b + " = " + c);
-	}
-	
-	public static void 뺄셈(int a, int b) {
-		int d = a - b;
-		System.out.println(a + " - " + b + " = " + d);
+		Calc calc = new Calc();
+		calc.덧셈(a,b);
+		calc.뺄셈(a,b);
 	}
 	
 }
