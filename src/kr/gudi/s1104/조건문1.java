a7b98f663c7cc94a43cadf4f098c011a5fba0468
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-04 17:51:49 +0900

    홀짝 문제 코드

--- src/kr/gudi/s1104/조건문1.java
+++ src/kr/gudi/s1104/조건문1.java
@@ -5,8 +5,24 @@ public class 조건문1 {
 	public static void main(String[] args) {
 		// 홀수면 [O] 짝수면 [X] 1 ~ 10
 		int 수 = 1;
-		
-		수++;
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+		홀짝확인용(수++);
+	}
+	
+	public static void 홀짝확인용(int 수) {
+		if(수%2 == 0) {
+			System.out.println(수 + " : [X]");
+		} else {
+			System.out.println(수 + " : [O]");
+		}
 	}
 	
 }
