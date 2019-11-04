76da7d1672faa63431657d4154ac50c4a6fdd6bb
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-04 17:32:01 +0900

    조건문 범위 설정 코드

--- src/kr/gudi/s1104/조건문1.java
+++ src/kr/gudi/s1104/조건문1.java
@@ -11,8 +11,27 @@ public class 조건문1 {
 		 * 
 		 * 입력 받은 값은 78점이면 학점은 무엇인가?
 		 ************************************/
-		
-		
+		int 입력값 = 101;
+//		if(입력값 > 100) {
+//			System.out.println("입력값은 잘못되었다.");
+//			return;
+//		} else if(입력값 < 0) {
+//			System.out.println("입력값은 잘못되었다.");
+//			return;
+//		}
+		if(입력값 > 90 && 입력값 <= 100) {
+			System.out.println("A학점");
+		} else if(입력값 > 80 && 입력값 <= 90) {
+			System.out.println("B학점");
+		} else if(입력값 > 70 && 입력값 <= 80) {
+			System.out.println("C학점");
+		} else if(입력값 > 60 && 입력값 <= 70) {
+			System.out.println("D학점");
+		} else if(입력값 >= 0 && 입력값 <= 60) {
+			System.out.println("F학점");
+		} else {
+			System.out.println("입력값은 잘못되었다.");
+		}
 		
 	}
 
