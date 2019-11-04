ee73f8d1d79ecc59c001b881e56fb6ae574a5862
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-04 17:04:42 +0900

    [문제] 입력 받은 값은 78점이면 학점은 무엇인지 조건문으로 표현하시오.

--- src/kr/gudi/s1104/조건문1.java
+++ src/kr/gudi/s1104/조건문1.java
@@ -3,21 +3,16 @@ package kr.gudi.s1104;
 public class 조건문1 {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 2;
-		int c = 5;
+		/* 100 ~ 91 : A학점
+		 * 90 ~ 81  : B학점 
+		 * 80 ~ 71  : C학점
+		 * 70 ~ 61  : D학점
+		 * 60 이하      : F학점
+		 * 
+		 * 입력 받은 값은 78점이면 학점은 무엇인가?
+		 ************************************/
 		
-		if((a / b) > c) {
-			System.out.println("결과값이 c보다 크다");
-		} 
 		
-		if((a / b) < c) {
-			System.out.println("결과값이 c보다 작다");
-		} 
-		
-		if((a / b) == c) {
-			System.out.println("두개의 값은 같다");
-		} 
 		
 	}
 
