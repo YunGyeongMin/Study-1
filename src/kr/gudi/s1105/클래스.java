3c7c1d311a6442a8bbdf7c5076c56c074ef6e738
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-05 16:03:44 +0900

    스위치 알아보기

---
+++ src/kr/gudi/s1105/클래스.java
@@ -0,0 +1,18 @@
+package kr.gudi.s1105;
+
+public class 클래스 {
+
+	public static void main(String[] args) {
+		// 조건문!! if문, switch문 중첩이 가능하다.
+		String a = "일어난다.";
+		String b = "씻는다.";
+		String c = "밥 먹는다.";
+		String d = "출근한다.";
+		String 조합값 = "";
+		
+		boolean 유형 = true;  // true (밥 먹는 사람) / false (밥 먹지 않는 사람)
+		
+		
+	}
+
+}
