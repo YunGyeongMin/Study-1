ecc23dd81a2e90c0f12c0b9720bee2bbf48c8098
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-06 14:48:21 +0900

    while문 코드 확인

--- src/kr/gudi/s1106/클래스.java
+++ src/kr/gudi/s1106/클래스.java
@@ -3,10 +3,21 @@ package kr.gudi.s1106;
 public class 클래스 {
 
 	public static void main(String[] args) {
-		for(int a=0; a<5; a++) 
+		
+		int a = 0;
+		for(;true;) 
 		{ 
+			if(a >= 5) break;
 			System.out.println("for문 : " + a);
-		}		
+			a++;
+		}
+		
+		a = 0;
+		while(true) {
+			if(a >= 5) break;
+			System.out.println("while문 : " + a);
+			a++;
+		}
 	}
 
 }
