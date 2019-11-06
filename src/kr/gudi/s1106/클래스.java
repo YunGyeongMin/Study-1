2f81e3383adfdd53fb260ca84b063fb76fd08882
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-06 15:52:17 +0900

    재귀 메소드 호출 방식으로 구구단 출력

--- src/kr/gudi/s1106/클래스.java
+++ src/kr/gudi/s1106/클래스.java
@@ -5,16 +5,24 @@ public class 클래스 {
 	public static void main(String[] args) {
 		
 		for(int i = 1; i <= 9; i++) {
-			
 			for(int j = 1; j <= 9; j++) {
-				
 				System.out.println(i + " * " + j + " = " + (i * j));
-				
 			}
-			
 		}
 		
-		
+//		단(1);
+	}
+	
+	public static void 단(int i) {
+		if(i > 9) return;
+		증가연산(i, 1);
+		단(++i);
+	}
+	
+	public static void 증가연산(int i, int j) {
+		if(j > 9) return;
+		System.out.println(i + " * " + j + " = " + (i * j));
+		증가연산(i, ++j);
 	}
 
 }
