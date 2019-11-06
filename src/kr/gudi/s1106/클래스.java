6a7e21a00392f86892911dd46c4f6375e0ecb01e
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-06 14:56:54 +0900

    중첩 for문를 이용한 구구단

--- src/kr/gudi/s1106/클래스.java
+++ src/kr/gudi/s1106/클래스.java
@@ -4,20 +4,17 @@ public class 클래스 {
 
 	public static void main(String[] args) {
 		
-		int a = 0;
-		for(;true;) 
-		{ 
-			if(a >= 5) break;
-			System.out.println("for문 : " + a);
-			a++;
+		for(int i = 1; i <= 9; i++) {
+			
+			for(int j = 1; j <= 9; j++) {
+				
+				System.out.println(i + " * " + j + " = " + (i * j));
+				
+			}
+			
 		}
 		
-		a = 0;
-		while(true) {
-			if(a >= 5) break;
-			System.out.println("while문 : " + a);
-			a++;
-		}
+		
 	}
 
 }
