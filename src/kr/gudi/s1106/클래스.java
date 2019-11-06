c87be6cc0a51f6ac0876071adf56f57bb8564558
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-06 17:01:33 +0900

    동적 구구단 코드

--- src/kr/gudi/s1106/클래스.java
+++ src/kr/gudi/s1106/클래스.java
@@ -1,30 +1,32 @@
 package kr.gudi.s1106;
 
+import java.util.Scanner;
+
 public class 클래스 {
 
 	public static void main(String[] args) {
 		
-		for(int i = 1; i <= 9; i = i + 2) {
-			for(int j = 1; j <= 9; j++) {
-				String a = i + " * " + j + " = " + (i * j);
-				String b = (i+1) + " * " + j + " = " + ((i+1) * j);
-				System.out.println(a + "\t" + b);
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			System.out.println("구구단 열의 갯수를 입력하세요.");
+			int 열 = scan.nextInt();
+			if(열 == 0) break;
+			for(int i = 1; i <= 9; i = i + 열) {
+				for(int j = 1; j <= 9; j++) {
+					String 행 = "";
+					for(int c = 0; c < 열; c++) {
+						if((i+c) > 9) break;
+						행 += (i+c) + " * " + j + " = " + ((i+c) * j) + "\t";
+					}
+	//				String a = (i+0) + " * " + j + " = " + ((i+0) * j);
+	//				String b = (i+1) + " * " + j + " = " + ((i+1) * j);
+	//				String c = (i+2) + " * " + j + " = " + ((i+2) * j);
+					System.out.println(행);
+					
+				}
 			}
 		}
 		
-//		단(1);
 	}
 	
-	public static void 단(int i) {
-		if(i > 9) return;
-		증가연산(i, 1);
-		단(++i);
-	}
-	
-	public static void 증가연산(int i, int j) {
-		if(j > 9) return;
-		System.out.println(i + " * " + j + " = " + (i * j));
-		증가연산(i, ++j);
-	}
-
 }
