a2f9b75526eed7d1de34e50485cdba2b279c5fb0
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-06 16:32:25 +0900

    구구단 출력 2단씩 만들기

--- src/kr/gudi/s1106/클래스.java
+++ src/kr/gudi/s1106/클래스.java
@@ -4,9 +4,11 @@ public class 클래스 {
 
 	public static void main(String[] args) {
 		
-		for(int i = 1; i <= 9; i++) {
+		for(int i = 1; i <= 9; i = i + 2) {
 			for(int j = 1; j <= 9; j++) {
-				System.out.println(i + " * " + j + " = " + (i * j));
+				String a = i + " * " + j + " = " + (i * j);
+				String b = (i+1) + " * " + j + " = " + ((i+1) * j);
+				System.out.println(a + "\t" + b);
 			}
 		}
 		
