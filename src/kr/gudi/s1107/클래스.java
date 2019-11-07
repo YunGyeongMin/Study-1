07f9bcdcbaaa6a1a5fe67703389cd4ab15a3590b
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-07 14:48:10 +0900

    [문제] 역삼각형 도형 출력 - 반복문

---
+++ src/kr/gudi/s1107/클래스.java
@@ -0,0 +1,17 @@
+package kr.gudi.s1107;
+
+public class 클래스 {
+
+	public static void main(String[] args) {
+		String 도형1 = "■";
+		String 도형2 = " ";
+		
+		System.out.println(도형1+도형1+도형1+도형1+도형1+도형1+도형1+도형1+도형1);
+		System.out.println(도형2+도형1+도형1+도형1+도형1+도형1+도형1+도형1+도형2);
+		System.out.println(도형2+도형2+도형1+도형1+도형1+도형1+도형1+도형2+도형2);
+		System.out.println(도형2+도형2+도형2+도형1+도형1+도형1+도형2+도형2+도형2);
+		System.out.println(도형2+도형2+도형2+도형2+도형1+도형2+도형2+도형2+도형2);
+		
+	}
+
+}
