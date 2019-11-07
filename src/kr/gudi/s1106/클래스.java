d1751e759171531dce59e9886863a6c04a6376a7
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-07 14:41:17 +0900

    도형 반복문 풀이

--- src/kr/gudi/s1106/클래스.java
+++ src/kr/gudi/s1106/클래스.java
@@ -6,15 +6,23 @@ public class 클래스 {
 		
 		String 도형 = "■";
 		
-		System.out.println(도형);
-		System.out.println(도형+도형);
-		System.out.println(도형+도형+도형);
-		System.out.println(도형+도형+도형+도형);
-		System.out.println(도형+도형+도형+도형+도형);
-		System.out.println(도형+도형+도형+도형);
-		System.out.println(도형+도형+도형);
-		System.out.println(도형+도형);
-		System.out.println(도형);
+//		System.out.println(도형);					// i = 0
+//		System.out.println(도형+도형);				// i = 1
+//		System.out.println(도형+도형+도형);			// i = 2
+//		System.out.println(도형+도형+도형+도형);		// i = 3
+//		System.out.println(도형+도형+도형+도형+도형);	// i = 4
+//		System.out.println(도형+도형+도형+도형);		// i = 5 > 역순 4번 = 9
+//		System.out.println(도형+도형+도형);			// i = 6 > 역순 3번 = 9 
+//		System.out.println(도형+도형);				// i = 7 > 역순 2번 = 9
+//		System.out.println(도형);					// i = 8 > 역순 1번 = 9
+		
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j <= i; j++) {
+				if((9 - i) <= j) break;
+				System.out.print(도형);
+			}
+			System.out.println("");
+		}		
 		
 	}
 	
